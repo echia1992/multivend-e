6c7997e4039df104cd9db279c59f85751c799f07
echia1992 <echiaanthony1@gmail.com>
2024-05-06 16:16:28 +0100

    initia

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true
 }));
 app.use('/', express.static('uploads'));
